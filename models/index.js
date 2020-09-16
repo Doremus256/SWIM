01346dbc32e7916c7eb1fcf5b084c43047a50517
Brad Doremus <rbdoremus13@gmail.com>
2020-09-15 21:58:22 -0500

    Fixed GET/POST api-routes and added DELETE

--- models/index.js
+++ models/index.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-require("dotenv").config();
-=======
 "use strict";
->>>>>>> a1c24c8294f26eb038dcf931abd85c51c6527d6f
 
 var fs = require("fs");
 var path = require("path");
@@ -13,33 +9,6 @@ var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
 if (config.use_env_variable) {
-<<<<<<< HEAD
-     var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-     var sequelize = new Sequelize(config.database, config.username, process.env.password, config);
-}
-   
-fs
-     .readdirSync(__dirname)
-     .filter(function(file) {
-          return (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js");
-     })
-     .forEach(function(file) {
-     var model = sequelize["import"](path.join(__dirname, file));
-     db[model.name] = model;
-});
-   
-Object.keys(db).forEach(function(modelName) {
-     if (db[modelName].associate) {
-          db[modelName].associate(db);
-     }
-});
-   
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-   
-module.exports = db;
-=======
     var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
     var sequelize = new Sequelize(config.database, config.username, config.password, config);
@@ -65,4 +34,3 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
->>>>>>> a1c24c8294f26eb038dcf931abd85c51c6527d6f
