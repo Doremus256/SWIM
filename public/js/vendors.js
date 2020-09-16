b117930e8cf94e3f1b603e3ec32d749f1f4d42b0
Brad Doremus <rbdoremus13@gmail.com>
2020-09-16 14:42:38 -0500

    pulling team updates on views / js files

--- public/js/vendors.js
+++ public/js/vendors.js
@@ -1,20 +1,7 @@
-$.ajax({
-    method: "GET",
-    data: data,
-    URL: "/api/vendors"
-}).then(function (response) {
-    console.log(response)
-})
-
-
-$("#updateBtn").on("click", function (event) {
+$("#sourceBtn").on("click", function (event) {
     event.preventDefault()
-    var id = $("#vendorDropdown").val()
-    var data = {
-        vendor_QOH: $("#vendor").val().trim(),
-    }
     $.ajax({
-        method: "PUT",
+        method: "GET",
         data: data,
         URL: "/api/vendors/" + id
     }).then(function (response) {
