89a9abbffe54086f1f53ddf91a0670376045420a
Kyle Gribble <dribbleGribble0516@gmail.com>
2020-09-16 11:32:30 -0500

    Updated mysql table sequelize scripts

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,12 +1,12 @@
 // DEPENDENCIES //
-// REQUIRING index.js by requiring models folder it lives in //
+// REQUIRING index.js by requiring models folder it lives in
 var db = require("../models");
 
 // ROUTES //
 
 module.exports = function (app) {
 
-    // GET route for getting all ITEMS in current inventory seeded on the backend //
+    // GET route for getting all ITEMS in current inventory seeded on the backend
     app.get("/api/items", function (req, res) {
         // sequelize findAll() so we get every row in the Items table
         db.Items.findAll({}).then(function (dbItems) {
@@ -14,11 +14,11 @@ module.exports = function (app) {
         });
     });
 
-    // PUT route for updating current inventory given :item //
+    // PUT route for updating current inventory given :item
     app.put("/api/items/:item_name", function (req, res) {
         console.log(req.body);
-        // sequelize update() so we can change item_QOH (quantity on hand) //
-        // of a single given item_name (selected from dropdown menu to avoid typos) //
+        // sequelize update() so we can change item_QOH (quantity on hand)
+        // of a single given item_name (selected from dropdown menu to avoid typos)
         db.Items.update({
             item_QOH: req.body.item_QOH,
         }, {
@@ -32,7 +32,7 @@ module.exports = function (app) {
 
     // DELETE route for deleting items from inventory (from items table within swim_db)
     app.delete("/api/items/:item_name", function (req, res) {
-        // sequelize destroy() so we can delete items completely from our items table //
+        // sequelize destroy() so we can delete items completely from our items table
         db.Items.destroy({
             where: {
                 item_name: req.body.item_name
